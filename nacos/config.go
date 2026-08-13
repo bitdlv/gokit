1c142cc0a2f319e070f5696e28f84b4ec96ffdaa
zhangyuliang <zhangyuliang156@163.com>
2026-08-13 15:43:14 +0800

    pb包修改

--- nacos/config.go
+++
@@ -1,7 +0,0 @@
-package nacos
-
-var NameSpaceMap = map[string]string{
-	"base": "3bf5e50b-424a-4011-afe8-2eea9ad3ee24",
-	"test": "7a329dcc-9e66-481c-b023-33bcdd668c3c",
-	"prod": "3e201505-44fc-4315-b8ef-dc66bc0c4f73",
-}
